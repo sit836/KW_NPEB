ccc7cc5b3f7e759a9fb1b78b5011705abd039867
SileTao <sile@quartic.ai>
2019-02-07 22:03:05 -0500

    slight change

--- simulation.py
+++ simulation.py
@@ -8,6 +8,6 @@ def simulate_discrete_prior(sz):
     :param sz: size of data
     :return: simulated data
     """
-    support_prior = [8, 12]
+    support_prior = [5, 15]
     signal = np.random.choice(support_prior, sz)
     return signal + np.random.normal(size=sz)
