eca3cc59bb49a0b6adb003899dbebe36d366ab06
SileTao <sile@quartic.ai>
2019-02-17 15:17:51 -0500

    slight changes

--- npeb_enc.py
+++ npeb_enc.py
@@ -1,17 +1,23 @@
 from kw_mle import KWMLE
 
-def npeb_enc(cat_features, label, target):
-    train = cat_features.join(target)
 
-    target_grouped = train.groupby(feature)[target]
+class NPEBEnc:
+    def __init__(self, categorical_features=None):
+        self.categorical_features = categorical_features
 
-    counts_grouped = target_grouped.count().values.flatten()
-    means_grouped = target_grouped.mean().values.flatten()
-    stds_grouped = target_grouped.std().values.flatten() / np.sqrt(counts_grouped)
+    def fit(self, categorical_features, label, target_name):
+        """
+        Fit OneHotEncoder to categorical_features.
+        """
+        for feature in cat_features:
+            target_grouped = train.groupby(feature)[target_name]
 
-    kw_mle = KWMLE(means_grouped, stds=stds_grouped)
-    kw_mle.fit()
+            counts_grouped = target_grouped.count().values.flatten()
+            means_grouped = target_grouped.mean().values.flatten()
+            stds_grouped = target_grouped.std().values.flatten() / np.sqrt(counts_grouped)
 
-    enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
-    enc_dict = dict(zip(target_grouped.mean().index, enc))
-    X_test[feature + '_enc'] = X_test[feature].map(enc_dict)
+            kw_mle = KWMLE(means_grouped, stds=stds_grouped)
+            kw_mle.fit()
+
+            enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
+            enc_dict = dict(zip(target_grouped.mean().index, enc))
