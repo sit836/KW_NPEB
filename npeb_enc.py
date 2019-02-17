a508ee9d43afbeaad6a1f36b3ea22ca11e304899
SileTao <sile@quartic.ai>
2019-02-17 15:33:25 -0500

    develope

--- npeb_enc.py
+++ npeb_enc.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from kw_mle import KWMLE
 
 
@@ -9,8 +11,10 @@ class NPEBEnc:
         """
         Fit OneHotEncoder to categorical_features.
         """
-        for feature in cat_features:
-            target_grouped = train.groupby(feature)[target_name]
+        df = categorical_features.join(label)
+
+        for feature in categorical_features.columns:
+            target_grouped = df.groupby(feature)[target_name]
 
             counts_grouped = target_grouped.count().values.flatten()
             means_grouped = target_grouped.mean().values.flatten()
@@ -20,4 +24,30 @@ class NPEBEnc:
             kw_mle.fit()
 
             enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
-            enc_dict = dict(zip(target_grouped.mean().index, enc))
+            self.enc_dict = dict(zip(target_grouped.mean().index, enc))
+
+    def transform(self, categorical_features):
+        transformed_features = pd.DataFrame(index=categorical_features.index)
+        for feature in categorical_features.columns:
+            transformed_features[feature + '_enc'] = categorical_features[feature].map(self.enc_dict)
+        return transformed_features
+
+
+import pandas as pd
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+boston = load_boston()
+df = pd.DataFrame(boston.data)
+df.columns = boston.feature_names
+df['PRICE'] = boston.target
+
+cat_names = ['CHAS', 'RAD']
+target_name = 'PRICE'
+
+X_train, X_test, y_train, y_test = train_test_split(df[cat_names], df[target_name], test_size=0.50, random_state=123)
+train = X_train.join(y_train)
+
+enc = NPEBEnc()
+enc.fit(X_train, y_train, target_name)
+print(enc.transform(X_test))
