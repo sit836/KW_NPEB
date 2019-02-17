5ad6eae2d4c1de719142e6fcf5b86c3836e5cd62
SileTao <sile@quartic.ai>
2019-02-17 11:26:19 -0500

    continue enc

--- explore_boston.py
+++ explore_boston.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
@@ -23,20 +24,29 @@ target = 'PRICE'
 feature = cat_features
 target_grouped = df.groupby(feature)[target]
 
-X_train, X_test, y_train, y_test = train_test_split(df[cat_features], df[target], test_size=0.33, random_state=123)
+X_train, X_test, y_train, y_test = train_test_split(df[cat_features], df[target], test_size=0.50, random_state=123)
 train = X_train.join(y_train)
 
 for feature in cat_features:
     target_grouped = train.groupby(feature)[target]
 
-    gp_stds = pd.DataFrame(train.groupby(feature)[target].std())
-    std_col = feature + '_GP_STDS'
-    gp_stds.columns = [std_col]
-    train_joined = train.set_index(feature).join(gp_stds)
+    counts_grouped = pd.DataFrame(target_grouped.count())
+    means_grouped = pd.DataFrame(target_grouped.mean())
+    stds_grouped_raw = pd.DataFrame(target_grouped.std())
+    stds_grouped = stds_grouped_raw / np.sqrt(counts_grouped)
+    std_col = feature + '_STDS_GP'
+    stds_grouped.columns = [std_col]
 
-    train_obs, train_stds = train_joined[target].values, train_joined[std_col].values
-
-    kw_mle = KWMLE(train_obs, stds=train_stds)
+    train_data, train_stds = means_grouped.values.flatten(), stds_grouped.values.flatten()
+    kw_mle = KWMLE(train_data, stds=train_stds)
     prior, mixture = kw_mle.kw_dual()
-    pred = kw_mle.prediction(train_obs, stds=train_stds)
-    train[feature + '_new'] = pred
+
+    train_enc_raw = kw_mle.prediction(train_data, stds=train_stds)
+    train_enc = pd.DataFrame(train_enc_raw, index=means_grouped.index, columns=[feature + '_enc'])
+
+    print("train_enc\n", train_enc)
+
+    test_data = pd.DataFrame(X_test[feature], index=X_test[feature], columns=[feature])
+    test_data.join(train_enc)
+    print("test_data.join(train_enc)\n", test_data.join(train_enc))
+
