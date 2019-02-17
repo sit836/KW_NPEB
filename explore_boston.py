34e09f79a50335075dcbb40b8cb2cb750c1b6a64
SileTao <sile@quartic.ai>
2019-02-17 17:16:38 -0500

    develope

--- explore_boston.py
+++ explore_boston.py
@@ -1,44 +1,34 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 
-from kw_mle import KWMLE
+from npeb_enc import NPEBEnc
+
+def hist_per_cat(df, cat_names, target_name):
+    for feature in cat_names:
+        target_grouped = df.groupby(feature)[target_name]
+        ncols = 2
+        nrows = int(np.ceil(target_grouped.ngroups / ncols))
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
+        for (key, ax) in zip(target_grouped.groups.keys(), axes.flatten()):
+            target_grouped.get_group(key).hist(ax=ax)
+            ax.set_title(f"{feature}_{key}")
+        ax.legend()
+    fig.subplots_adjust(hspace=0.5)
+    plt.show()
 
 boston = load_boston()
 df = pd.DataFrame(boston.data)
 df.columns = boston.feature_names
 df['PRICE'] = boston.target
+cat_names = ['CHAS', 'RAD']
+target_name = 'PRICE'
 
-# import matplotlib.pyplot as plt
-# feature, target = 'CHAS', 'PRICE'
-# target_grouped = df.groupby(feature)[target]
-# ncols = 2
-# nrows = int(np.ceil(target_grouped.ngroups / ncols))
-# fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
-# for (key, ax) in zip(target_grouped.groups.keys(), axes.flatten()):
-#     target_grouped.get_group(key).hist(ax=ax)
-# ax.legend()
-# plt.show()
+hist_per_cat(df, cat_names, target_name)
 
-cat_features = ['CHAS', 'RAD']
-target = 'PRICE'
-
-X_train, X_test, y_train, y_test = train_test_split(df[cat_features], df[target], test_size=0.50, random_state=123)
-train = X_train.join(y_train)
-
-for feature in cat_features:
-    target_grouped = train.groupby(feature)[target]
-
-    counts_grouped = target_grouped.count().values.flatten()
-    means_grouped = target_grouped.mean().values.flatten()
-    stds_grouped = target_grouped.std().values.flatten() / np.sqrt(counts_grouped)
-
-    kw_mle = KWMLE(means_grouped, stds=stds_grouped)
-    kw_mle.fit()
-
-    train_enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
-    enc_dict = dict(zip(target_grouped.mean().index, train_enc))
-    X_test[feature + '_enc'] = X_test[feature].map(enc_dict)
-
-print(X_test)
+X_train, X_test, y_train, y_test = train_test_split(df[cat_names], df[target_name], test_size=0.50, random_state=123)
+enc = NPEBEnc()
+enc.fit(X_train, y_train, target_name)
+print(enc.transform(X_test))
--- npeb_enc.py
+++ npeb_enc.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 
 from kw_mle import KWMLE
 
 
 class NPEBEnc:
-    def __init__(self, categorical_features=None):
-        self.categorical_features = categorical_features
+    def __init__(self):
+        self.enc_dicts = {}
 
     def fit(self, categorical_features, label, target_name):
         """
@@ -24,30 +25,10 @@ class NPEBEnc:
             kw_mle.fit()
 
             enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
-            self.enc_dict = dict(zip(target_grouped.mean().index, enc))
+            self.enc_dicts[feature] = dict(zip(target_grouped.mean().index, enc))
 
     def transform(self, categorical_features):
         transformed_features = pd.DataFrame(index=categorical_features.index)
         for feature in categorical_features.columns:
-            transformed_features[feature + '_enc'] = categorical_features[feature].map(self.enc_dict)
+            transformed_features[feature + '_enc'] = categorical_features[feature].map(self.enc_dicts[feature])
         return transformed_features
-
-
-import pandas as pd
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
-
-boston = load_boston()
-df = pd.DataFrame(boston.data)
-df.columns = boston.feature_names
-df['PRICE'] = boston.target
-
-cat_names = ['CHAS', 'RAD']
-target_name = 'PRICE'
-
-X_train, X_test, y_train, y_test = train_test_split(df[cat_names], df[target_name], test_size=0.50, random_state=123)
-train = X_train.join(y_train)
-
-enc = NPEBEnc()
-enc.fit(X_train, y_train, target_name)
-print(enc.transform(X_test))
