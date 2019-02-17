6a0bc13b53c295e1c348a7e619bfb78df277d98a
SileTao <sile@quartic.ai>
2019-02-17 14:26:41 -0500

    simplify codes

--- explore_boston.py
+++ explore_boston.py
@@ -10,6 +10,9 @@ df = pd.DataFrame(boston.data)
 df.columns = boston.feature_names
 df['PRICE'] = boston.target
 
+# import matplotlib.pyplot as plt
+# feature, target = 'CHAS', 'PRICE'
+# target_grouped = df.groupby(feature)[target]
 # ncols = 2
 # nrows = int(np.ceil(target_grouped.ngroups / ncols))
 # fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
@@ -21,28 +24,21 @@ df['PRICE'] = boston.target
 cat_features = ['CHAS', 'RAD']
 target = 'PRICE'
 
-feature = cat_features
-target_grouped = df.groupby(feature)[target]
-
 X_train, X_test, y_train, y_test = train_test_split(df[cat_features], df[target], test_size=0.50, random_state=123)
 train = X_train.join(y_train)
 
 for feature in cat_features:
     target_grouped = train.groupby(feature)[target]
 
-    counts_grouped = pd.DataFrame(target_grouped.count())
-    means_grouped = pd.DataFrame(target_grouped.mean())
-    stds_grouped_raw = pd.DataFrame(target_grouped.std())
-    stds_grouped = stds_grouped_raw / np.sqrt(counts_grouped)
-    std_col = feature + '_STDS_GP'
-    stds_grouped.columns = [std_col]
+    counts_grouped = target_grouped.count().values.flatten()
+    means_grouped = target_grouped.mean().values.flatten()
+    stds_grouped = target_grouped.std().values.flatten() / np.sqrt(counts_grouped)
 
-    train_data, train_stds = means_grouped.values.flatten(), stds_grouped.values.flatten()
-    kw_mle = KWMLE(train_data, stds=train_stds)
+    kw_mle = KWMLE(means_grouped, stds=stds_grouped)
     prior, mixture = kw_mle.kw_dual()
 
-    train_enc = kw_mle.prediction(train_data, stds=train_stds)
-    enc_dict = dict(zip(means_grouped.index, train_enc))
+    train_enc = kw_mle.prediction(means_grouped, stds=stds_grouped)
+    enc_dict = dict(zip(target_grouped.mean().index, train_enc))
     X_test[feature + '_enc'] = X_test[feature].map(enc_dict)
 
 print(X_test)
