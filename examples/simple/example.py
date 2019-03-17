b699cda1373f9b635c15c3cd96ad8529ef4121c6
SileTao <sile@quartic.ai>
2019-03-17 13:05:57 -0400

    restructure

--- examples/simple/example.py
+++ examples/simple/example.py
@@ -1,6 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from numpy.random import normal
 
-from plot import *
 from kw_mle import KWMLE
 
 sz = 1000
@@ -8,7 +9,18 @@ data = normal(loc=10, size=sz) + normal(size=sz)
 stds = [1]*len(data)
 kw_mle = KWMLE(data, stds=stds)
 kw_mle.fit()
+pred = kw_mle.prediction(np.sort(data), stds) # for visualization, data is sorted
 
-plot_prior(kw_mle)
-plot_mixture(kw_mle)
-plot_prediction(kw_mle, data, stds)
+plt.subplot(2, 2, 1)
+plt.plot(kw_mle.grid_of_mean, kw_mle.prior)
+plt.title("Estimated prior")
+
+plt.subplot(2, 2, 2)
+mixture_sorted = kw_mle.mixture[np.argsort(kw_mle.data)]
+plt.plot(np.sort(data), mixture_sorted, '-o')
+plt.title("Estimated mixture")
+
+plt.subplot(2, 2, 3)
+plt.plot(np.sort(data), pred, '-o')
+plt.title("Prediction")
+plt.show()
