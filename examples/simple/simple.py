3549851ac4eaa9063a31c4d05df69a5424092e41
SileTao <sile@quartic.ai>
2019-03-27 12:13:42 -0400

    update readme and plot

--- examples/simple/simple.py
+++ examples/simple/simple.py
@@ -11,16 +11,15 @@ kw_mle = KWMLE(data, stds=stds)
 kw_mle.fit()
 pred = kw_mle.prediction(np.sort(data), stds) # for visualization, data is sorted
 
-plt.subplot(2, 2, 1)
 plt.plot(kw_mle.grid_of_mean, kw_mle.prior)
 plt.title("Estimated prior")
+plt.show()
 
-plt.subplot(2, 2, 2)
 mixture_sorted = kw_mle.mixture[np.argsort(kw_mle.data)]
 plt.plot(np.sort(data), mixture_sorted, '-o')
 plt.title("Estimated mixture")
+plt.show()
 
-plt.subplot(2, 2, 3)
 plt.plot(np.sort(data), pred, '-o')
 plt.title("Prediction")
 plt.show()
